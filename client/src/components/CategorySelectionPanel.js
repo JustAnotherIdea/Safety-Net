aeeebdc86157120b9e243ea19233d3b59fec45e8
Noah LaLonde <33038393+JustAnotherIdea@users.noreply.github.com>
2024-10-13 00:32:55 -0500

    s

--- client/src/components/CategorySelectionPanel.js
+++ client/src/components/CategorySelectionPanel.js
@@ -13,7 +13,7 @@ function CategorySelectionPanel({ categories, selectedCategory, setSelectedCateg
   };
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-1 p-1">
       {internalSelectedCategory ? (
         <>
           <button
